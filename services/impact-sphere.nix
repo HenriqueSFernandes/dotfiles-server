5106d7711abee8b6543f2dd3d98d8d0e66c05553
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2026-04-03 11:25:49 +0100

    updated impact sphere website service name

--- services/impact-sphere.nix
+++ services/impact-sphere.nix
@@ -4,7 +4,7 @@
     impact-sphere.settings = {
       services = {
         website.service = {
-          image = "registry.henriquesf.me/impact-sphere:d2fc05b3d47f2702b284820a17e54ef2ec78f4c3";
+          image = "registry.henriquesf.me/impact-sphere-site:d2fc05b3d47f2702b284820a17e54ef2ec78f4c3";
           restart = "unless-stopped";
           ports = [ "127.0.0.1:8084:3000" ];
         };
